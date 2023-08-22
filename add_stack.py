9a85f36df9156d2731172999cd6c23a1d0e0a670
Artur Tomczyk <tomczyk.art@gmail.com>
2023-08-22 22:14:49 +0200

    First prototype

---
+++ add_stack.py
@@ -0,0 +1,20 @@
+"""This is first prototype of a game of stack"""
+
+from collections import namedtuple
+import json
+
+
+# Each stack will be a dict with name and tuple with digits as 6 walls
+def choose_stack(name, walls):
+    if isinstance(name, str) and isinstance(walls, tuple) and len(walls) == 6:
+        return {name: walls}
+
+
+# Lets keep stack in a file
+def save_stack(stack):
+    with open("data.json", "w") as f:
+        json.dump(stack, f)
+
+
+stack1 = choose_stack("stack1", (1, 3, 3, 4, 5, 9))
+save_stack(stack1)
