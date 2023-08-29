1bc4f3cb05ac8f333f51ec085161cfe77881bae4
Artur Tomczyk <tomczyk.art@gmail.com>
2023-08-29 15:25:27 +0200

    Upgrades on page layout.

--- stackapp/static/script.js
+++ stackapp/static/script.js
@@ -3,28 +3,58 @@ const canvas = document.getElementById('cubeCanvas');
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+
+// Update window size
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.updateProjectionMatrix()
+    camera.aspect = sizes.width / sizes.height;
+    renderer.setSize(sizes.width, sizes.height)
+})
+
 // Create a Three.js scene
 const scene = new THREE.Scene();
 
 // Create a camera and position it
-const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 50);
-camera.position.z = 2;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 50);
+// camera.position.z = 4;
+camera.position.set(1,0,4);
 
 // Create light
 const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set(0, 10, 10)
+light.position.set(0, 10, 15)
 scene.add(light)
 
 // Create a WebGL renderer
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
+// Set renderer size
+renderer.setSize(sizes.width, sizes.height)
+
 // Set canvas background color to white
 renderer.setClearColor(0xffffff);
 
+// Load textures
+const loader = new THREE.TextureLoader();
+const texture = loader.load( 'jeden.png' );
+texture.colorSpace = THREE.SRGBColorSpace;
+ 
+
 // Create a cube and add it to the scene
 const geometry = new THREE.BoxGeometry(1, 1, 1, 128, 128, 128);
-const material = new THREE.MeshStandardMaterial({ color: 0x44aa88 });
-//const cube = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshStandardMaterial({
+    color: 0x44aa88,
+    map: texture,
+ });
+
+
 
 // Create an array to store materials for each face
 const materials = [
@@ -71,6 +101,7 @@ function animate() {
     // Rotate the cube
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
 
     // Render the scene with the camera
     renderer.render(scene, camera);
